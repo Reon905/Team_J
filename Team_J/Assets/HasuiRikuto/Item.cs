31cdbe1a2d333faadf874cc5e8e9200f6169b8b2
hasuirikuto <h1202519006@hiratagakuen.onmicrosoft.com>
2025-10-17 12:24:37 +0900

    値段をランダム化したItemをprefabに追加　　蓮井

---
+++ Team_J/Assets/HasuiRikuto/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // アイテムの金額
+    public int price;
+
+    void Start()
+    {
+        // 1000円〜2000円の範囲でランダムな値を設定
+        price = Random.Range(2000, 4001);
+
+        Debug.Log("アイテムの金額は " + price + " 円です。");
+    }
+}
---
+++ Team_J/Assets/HasuiRikuto/Item3.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Item3 : MonoBehaviour
+{
+    // アイテムの金額
+    public int price;
+
+    void Start()
+    {
+        // 1000円〜2000円の範囲でランダムな値を設定（2000を含めたい場合は +1）
+        price = Random.Range(5000, 10001);
+
+        Debug.Log("アイテムの金額は " + price + " 円です。");
+    }
+}
