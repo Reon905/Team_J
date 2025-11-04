afc6df6bdf80abb24782e3bd935802327d723df3
hasuirikuto <h1202519006@hiratagakuen.onmicrosoft.com>
2025-11-04 10:50:06 +0900

    アイテムスクリプト追加　　蓮井

---
+++ Team_J/Assets/HasuiRikuto/Script/Item5.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Item5 : MonoBehaviour
+{
+    public int price;
+    public static int totalMoney = 0;
+    public static int totalPoints = 0;
+    public static int itemCount = 0;
+
+    private ItemManager itemManager;
+
+    void Start()
+    {
+        price = 10000;
+        itemManager = GetComponent<ItemManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            totalMoney += price;
+            totalPoints += 50;
+            itemCount++;
+
+            Debug.Log("アイテム5取得！ +" + price + "円、+50pt");
+
+            // ここでItemManagerに「取得済み」と伝える
+            if (itemManager != null)
+                itemManager.CollectItem();
+        }
+    }
+}
---
+++ Team_J/Assets/HasuiRikuto/Script/Item9.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Item9 : MonoBehaviour
+{
+    public int price;
+    public static int totalMoney = 0;
+    public static int totalPoints = 0;
+    public static int itemCount = 0;
+
+    private ItemManager itemManager;
+
+    void Start()
+    {
+        price = 30000;
+        itemManager = GetComponent<ItemManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            totalMoney += price;
+            totalPoints += 20;
+            itemCount++;
+
+            Debug.Log("アイテム4取得！ +" + price + "円、+70pt");
+
+            // ここでItemManagerに「取得済み」と伝える
+            if (itemManager != null)
+                itemManager.CollectItem();
+        }
+    }
+}
