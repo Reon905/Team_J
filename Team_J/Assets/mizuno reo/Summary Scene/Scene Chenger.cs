a30c04744732b276983df502523585aae2f2a0df
mizuno reo <h1202515036@hiratagakuen.onmicrosoft.com>
2025-11-19 09:35:09 +0900

    水野：あらすじの切り替え追加(再)

--- Team_J/Assets/mizuno reo/Summary Scene/Scene Chenger.cs
+++ Team_J/Assets/mizuno reo/Summary Scene/Scene Chenger.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
-
-public class SceneChenger
+using UnityEngine.SceneManagement;
+public class SceneChenger : MonoBehaviour 
 {
-    
+    private void Update()
+    {
+        //エンターを押したらシーン切り替え
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Indoor Scene");//室内に切り替え
+        }
+    }
 }
