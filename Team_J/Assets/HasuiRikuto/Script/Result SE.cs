71acbe894eed90c07b36821e5c8a30cf27cf59ba
mizuno reo <h1202515036@hiratagakuen.onmicrosoft.com>
2025-11-19 12:34:15 +0900

    水野：リザルトのSE追加

---
+++ Team_J/Assets/HasuiRikuto/Script/Result SE.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ResultSE : MonoBehaviour 
+{
+    public AudioSource audioSource;
+
+    //ƒ‰ƒ“ƒN‚²‚Æ‚ÉŒø‰Ê‰¹‚ğİ’è
+    public AudioClip sRankSE;
+    public AudioClip aRankSE; 
+    public AudioClip bRankSE;
+    public AudioClip cRankSE;
+    public AudioClip dRankSE;
+
+    //ƒ‰ƒ“ƒN‚ğŠO•”‚©‚çó‚¯æ‚é
+    public void PlayRankSound(string rank)
+    {
+        switch (rank)
+        {
+
+        }
+    }
+}
