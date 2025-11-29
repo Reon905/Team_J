7347eb72c9e1bd27526985dc9caa17557a3cf398
edakura taisei <edamame15796@gmail.com>
2025-11-29 15:02:13 +0900

    ドット調のフォントファイルを追加（パブリックドメインなので、著作権表記などは不要）

---
+++ Team_J/Assets/Scenes/Test/EdakuraTaisei/SuccessAmountDisplay.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuccessAmountDisplay : MonoBehaviour
+{
+    [SerializeField] private Text moneyText;
+
+    void Start()
+    {
+        int totalMoney = Item.totalMoney + Item2.totalMoney + Item3.totalMoney + Item4.totalMoney + Item5.totalMoney + Item6.totalMoney + Item7.totalMoney + Item8.totalMoney + Item9.totalMoney + Item10.totalMoney + Item11.totalMoney + Item12.totalMoney;
+        moneyText.text = $"èäéùã‡ {totalMoney}â~";
+
+    }
+}
