06f8bbc801fe5a1c616eed883bf4eed2db592bb8
edakura taisei <edamame15796@gmail.com>
2025-12-01 14:07:12 +0900

    NPCの視野を拡大

--- Team_J/Assets/Scenes/Test/EdakuraTaisei/Constants.cs
+++ Team_J/Assets/Scenes/Test/EdakuraTaisei/Constants.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class Constants
 {
     //NPC
-    public static float DEFAULT_SIGHT_ANGLE = 40.0f;    //NPCの視界範囲
+    public static float DEFAULT_SIGHT_ANGLE = 45.0f;    //NPCの視界範囲
     public static float DEFAULT_DETECTION_VALUE = 0.0f; //初期発覚値
     public static float MAX_DETECTION_VALUE = 2.0f;     //最大発覚値
 
