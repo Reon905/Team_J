81f03967884e18f595174e624850ce248af51ac7
edakura taisei <edamame15796@gmail.com>
2025-12-01 16:58:29 +0900

    脱出成功画面でポイントを表示するように変更

--- Team_J/Assets/Scenes/Test/EdakuraTaisei/SuccessAmountDisplay.cs
+++ Team_J/Assets/Scenes/Test/EdakuraTaisei/SuccessAmountDisplay.cs
@@ -5,10 +5,13 @@ public class SuccessAmountDisplay : MonoBehaviour
 {
     [SerializeField] private Text moneyText;
 
+
     void Start()
     {
         int totalMoney = Item.totalMoney + Item2.totalMoney + Item3.totalMoney + Item4.totalMoney + Item5.totalMoney + Item6.totalMoney + Item7.totalMoney + Item8.totalMoney + Item9.totalMoney + Item10.totalMoney + Item11.totalMoney + Item12.totalMoney;
-        moneyText.text = $"èäéùã‡ {totalMoney}â~";
+        int totalPoint = Money.totalPoints;
+        moneyText.text = $"èäéùã‡ {totalMoney}â~\n" +
+                         $"çáåvPt {Money.totalPoints}pt" ;
 
     }
 }
