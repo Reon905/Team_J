ab12d138751ab6481bf7ec7934a8eb43b58e3d55
hasuirikuto <h1202519006@hiratagakuen.onmicrosoft.com>
2025-12-02 10:14:17 +0900

    adad

--- Team_J/Assets/Image/HasuiRikuto/Script/Money.cs
+++ Team_J/Assets/Image/HasuiRikuto/Script/Money.cs
@@ -11,13 +11,18 @@ public class Money : MonoBehaviour
 
     public Text moneyText; // UI TextをInspectorにドラッグ
 
+    void Start()
+    {
+        int racePoints = PlayerPrefs.GetInt("TotalRacePoints", 0);
+        Money.totalPoints += racePoints;
+    }
     void Update()
     {
         // 各アイテムスクリプトの合計金額を取得
         DayMoney = Item.totalMoney + Item2.totalMoney + Item3.totalMoney + Item4.totalMoney + Item5.totalMoney + Item6.totalMoney + Item7.totalMoney + Item8.totalMoney + Item9.totalMoney + Item10.totalMoney + Item11.totalMoney + Item12.totalMoney;
 
         // 各アイテムスクリプトの合計ポイントを取得
-        DayPoint = Item.totalPoints + Item2.totalPoints + Item3.totalPoints + Item4.totalPoints + Item5.totalPoints + Item6.totalPoints + Item7.totalPoints + Item8.totalPoints + Item9.totalPoints + Item10.totalPoints + Item11.totalPoints + Item12.totalPoints;
+        DayPoint = Item.totalPoints + Item2.totalPoints + Item3.totalPoints + Item4.totalPoints + Item5.totalPoints + Item6.totalPoints + Item7.totalPoints + Item8.totalPoints + Item9.totalPoints + Item10.totalPoints + Item11.totalPoints + Item12.totalPoints + Money.totalPoints;
         
         
         moneyText.text =
