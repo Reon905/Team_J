0f63e01fd9a2fa9fbc0466f9c732901abed8c0cf
mizuno reo <h1202515036@hiratagakuen.onmicrosoft.com>
2025-12-19 11:19:44 +0900

    mizuno :Daymaneyに変更

--- Team_J/Assets/mizuno reo/Customize/MoneyDisplay.cs
+++ Team_J/Assets/mizuno reo/Customize/MoneyDisplay.cs
@@ -8,7 +8,7 @@ public class MoneyDisplay : MonoBehaviour
     void Start()
     {
         //int totalMoney = Item.totalMoney + Item2.totalMoney + Item3.totalMoney + Item4.totalMoney + Item5.totalMoney + Item6.totalMoney + Item7.totalMoney + Item8.totalMoney + Item9.totalMoney + Item10.totalMoney + Item11.totalMoney + Item12.totalMoney;
-        moneyText.text = $"èäéùã‡: {Money.Instance.totalMoney}â~";
-        Debug.Log(Money.Instance.totalMoney);
+        moneyText.text = $"èäéùã‡: {Money.Instance.DayMoney}â~";
+        Debug.Log(Money.Instance.DayMoney);
     }
 }
